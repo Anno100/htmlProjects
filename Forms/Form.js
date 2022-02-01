ed92a4d710ba0fe29dc96c0f516152307985cb16
Anno100 <alessmon.acoro@googlemail.com>
2022-02-01 21:23:14 +0100

    tryCatch block in Methode umgeschrieben

--- Forms/Form.js
+++ Forms/Form.js
@@ -1,29 +1,13 @@
-class Form {
+class Form{
     constructor(x = 0, y = 0, color = "black") {
+
         this.x = x;
         this.y = y;
         this.color = color;
         this.visible = true;
 
-        try { c } 
-        catch (err) {
-            console.error(err +
-                "\nCanvas Objekt wurde nie initialisiert." +
+        Form.try_catch();
 
-                "\n\nFügen sie am Anfang des Codes folgendes hinzu:" +
-                "\nvar c = document.getElementById(\"myCanvas\")" + 
-
-                "\n\nFalls diese Meldung immernoch angezeigt wird fügen sie im body-Tag ein Canvas-Tag mit der id \"myCanvas\" hinzu!!!");
-        }try { g } 
-        catch (err) {
-            console.error(err +
-                "\nGraphics Objekt wurde nie initialisiert." +
-
-                "\n\nFügen sie am Anfang des Codes folgendes hinzu:" +
-                "\nvar g = c.getContext(\"2d\")");
-
-        }
-        
     }
 
     getX = () => this.x;
@@ -38,5 +22,25 @@ class Form {
     getVisable = () => this.visible;
     setVisable = (visible) => { this.visible = visible; }
 
+    static try_catch = () =>{
+        try { c } 
+        catch (err) {
+            console.error(err +
+                "\nCanvas Objekt wurde nie initialisiert." +
+
+                "\n\nFügen sie am Anfang des Codes folgendes hinzu:" +
+                "\nvar c = document.getElementById(\"myCanvas\")" + 
+
+                "\n\nFalls diese Meldung immernoch angezeigt wird fügen sie im body-Tag ein Canvas-Tag mit der id \"myCanvas\" hinzu!!!");
+        }try { g } 
+        catch (err) {
+            console.error(
+                "\nGraphics Objekt wurde nie initialisiert." +
+
+                "\n\nFügen sie am Anfang des Codes folgendes hinzu:" +
+                "\nvar g = c.getContext(\"2d\")");
+
+        }
+    }
 
 }
