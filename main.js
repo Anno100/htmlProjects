077b47a1ed2a81373dc35fcada5e3017784c42ca
Anno100 <alessmon.acoro@googlemail.com>
2022-02-01 16:49:23 +0100

    Einfügen des innerHTML vom default-Script-Tag

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 //Erstelle eine js-Datei die den script-tag in main.html erstetzt
+
+document.body.style.margin = 0;
+
+var c = document.getElementById("myCanvas");
+c.width = window.innerWidth;
+c.height = window.innerHeight;
+
+var g = c.getContext("2d");
+g.fillRect(0,0,10,10);
+
+var R = new Rectangle();
