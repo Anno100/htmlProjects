d8ad208c4ef88f715e05c9dcaf83eabb128fce8f
Anno100 <alessmon.acoro@googlemail.com>
2022-02-01 17:18:39 +0100

    exceptions für c und g von Rectangle verschieben

--- Forms/Form.js
+++ Forms/Form.js
@@ -3,6 +3,36 @@ class Form{
         this.x = x;
         this.y = y;
         this.color = color;
-        this.visible = visible;
+        this.visible = true;
+        
+        try {
+            c;
+        } catch (err) {
+            console.error(err +
+                "\nCanvas Objekt wurde nie initialisiert." +
+
+                "\n\nFügen sie am Anfang des Codes folgendes hinzu:" +
+                "\nvar c = document.getElementById(\"myCanvas\")" +
+
+                "\n\nFalls diese Meldung immernoch angezeigt wird fügen sie im body-Tag ein Canvas-Tag mit der id \"myCanvas\" hinzu!!!"
+
+            );
+
+        }
+
+        try {
+            g;
+        } catch (err) {
+            console.error(err +
+                "\nGraphics Objekt wurde nie initialisiert." +
+
+                "\n\nFügen sie am Anfang des Codes folgendes hinzu:" +
+                "\nvar g = c.getContext(\"2d\")");
+
+        }
+    }
+    
+    setVisability = (visable) => {
+        this.visable = visable;
     }
 }
