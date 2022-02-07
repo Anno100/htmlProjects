8bfe6ac4fc12589996b15bf75a0256d23fd75981
Anno100 <alessmon.acoro@googlemail.com>
2022-02-07 14:35:05 +0100

    add Pix-Class with a lot of methods

---
+++ Forms/Pix.js
@@ -0,0 +1,36 @@
+class Pix{
+    constructor(pixMatrix, pixLength = 1){
+        this.x = 0;
+        this.y = 0;
+        this.pixMatrix = pixMatrix;
+        this.pixLength = pixLength;
+        this.colorSettings = (number) => {
+            switch(number){
+                case 1: g.fillStyle = Color.Black;
+                break;
+                case 2: g.fillStyle = Color.Red;
+                break;
+                case 3: g.fillStyle = Color.Yellow;
+                break;
+            }
+        };
+    }
+
+    setColorSettings = (newColorSettings) =>{
+        this.colorSettings = newColorSettings;
+    }
+    
+    draw = () => {
+        let x = 0;
+        let y = 0;
+        this.pixMatrix.forEach(pixArray => {
+            pixArray.forEach(pix => {
+                this.colorSettings(pix);
+                if(pix != 0) g.fillRect(this.x + x * this.pixLength, this.y + y * this.pixLength, this.pixLength,this.pixLength);
+                x++;
+            });
+            x = 0;
+            y++;
+        });
+    }
+}
