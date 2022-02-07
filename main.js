bab3268d7925a596bdea29b33cde340ed274051c
Anno100 <alessmon.acoro@googlemail.com>
2022-02-07 14:08:44 +0100

    ...

--- main.js
+++ main.js
@@ -3,11 +3,31 @@
 document.body.style.margin = 0;
 
 var c = document.getElementById("myCanvas");
+window.innerWidth = window.screen.width;
+window.innerHeight = window.screen.height;
+
+
 c.width = window.innerWidth;
 c.height = window.innerHeight;
 
 var g = c.getContext("2d");
 
-var R = new Rectangle(10,10,100,100,Color.RGB(255,0,0,0.5));
+r1 = new Rectangle(10,10,100,100);
+r2 = new Rectangle(600,60,10,10);
 
-R.fill();
+Timer = () =>{
+    r1.x += 0.1;
+    if(r1.overlap(r2)) alert("YAY");
+}
+
+GUI = () =>{
+    g.clearRect(0,0,c.width,c.height);
+    r1.fill();
+    r2.fill();
+}
+
+setInterval(()=>{
+    GUI();
+    Timer();
+
+},1);
