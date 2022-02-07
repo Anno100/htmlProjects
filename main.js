c164c0a7e93a74f778d6f9ea2bfdae32659d70c0
Anno100 <alessmon.acoro@googlemail.com>
2022-02-07 15:27:23 +0100

    ...

--- main.js
+++ main.js
@@ -13,20 +13,31 @@ c.height = window.innerHeight;
 var g = c.getContext("2d");
 
 
-var p = new Pix([
+var p1 = new Pix([
     
     [0,1,0],
-    [0,2,0],
-    [0,3,0]
+    [0,1,0],
+    [0,1,0]
 
-],10);
+]);
+var p2 = new Pix([
+    
+    [0,0,0],
+    [1,1,1],
+    [0,0,0]
+
+]);
+
+var p = new PixSet([p1,p2],100);
+p.run = true;
 
 
 Timer = () =>{
 }
 
 GUI = () =>{
-    p.draw();
+    g.clearRect(0,0,c.width,c.height);
+    p.draw(100,100,10);
 }
 
 setInterval(()=>{
